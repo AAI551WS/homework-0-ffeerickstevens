6dfcdca335b4e09cfcb7a798d3c1336a80b8a860
Fiona Feerick <ffeerick@stevens.edu>
2024-01-18 19:15:28 -0500

    I made edits in vscode, it is complete

--- answer.py
+++ answer.py
@@ -2,16 +2,18 @@ def hello_world():
     # Write a doc string below. Doc strings can be used to provide the user with a
     # description of what the function does.
     """
-
+In this assignment, the function hello_world that I will be writing will print Hello, World! and the function add will add the two values of a and b.
     """
 
     # Write a comment; comments are very useful to other programmers.
+    #Hi, this is my comment line.
 
     # Write a Python statement to print Hello, World!
+print("Hello, World!")
 
 def add(a,b):
 
     # Write simple code to add variable a and b
-    output =
+    output = a + b
 
     return output
